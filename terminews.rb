34698f3dc5980c7fe36273fdf5f631c23d4aface
Richard Julian <richard@rjulian.net>
2025-02-20 11:30:40 +0100

    Make the title special

--- terminews.rb
+++ terminews.rb
@@ -24,7 +24,11 @@ MAX_ROWS.times do |row|
     left = time * width
     boxed_headline = full_window.subwin(height, width, top, left)
     boxed_headline.bkgd(Curses.color_pair(1))
-    boxed_headline.box('+', '-')
+    if time.zero? && row.zero?
+      boxed_headline.box('*', '*')
+    else
+      boxed_headline.box('+', '-')
+    end
     boxed_headline.refresh
     background_array = [Curses.color_pair(3), Curses.color_pair(2), Curses.color_pair(1), Curses.color_pair(4)]
     text_area_window = boxed_headline.subwin(height - 4, width - 4, top + 2, left + 2)
